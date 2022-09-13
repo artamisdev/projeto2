ae0d7bc3c0354180422e4a4f4dee6b06bbc59880
Tamires Braga <tamirescamposbraga@gmail.com>
2022-09-13 09:37:54 -0300

    organizando

--- src/pages/Profiles/index.js
+++ src/pages/Profiles/index.js
@@ -1,13 +1,13 @@
 // PAGINA COM TODOS OS PERFIS DOS USERS E O FORM PARA CADASTRO
 import NavBar from "../../components/NavBar";
 import Profile from "../Profile";
-import axios from "axios"
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Button, Form, Row, Col, Card } from "react-bootstrap";
 
 function Profiles() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState([]);
 
   const [search, setSearch] = useState("");
 
@@ -32,66 +32,67 @@ function Profiles() {
 
   return (
     <>
-    <NavBar/>
+      <NavBar />
 
-    <div>
-      <Row>
-        <Col className="col-10">
-          <Form.Control
-            value={search}
-            onChange={handleSearch}
-            placeholder="Procure um companheiro jardineiro"
-          />
-        </Col>
+      <div>
+        <Row>
+          <Col className="col-10">
+            <Form.Control
+              value={search}
+              onChange={handleSearch}
+              placeholder="Procure um companheiro jardineiro"
+            />
+          </Col>
 
-        <Col>
-          <Link
-            to="/user-profile"
-            >
-            <Button className="btn btn-light btn-outline-dark">
-              Crie Perfil
-            </Button>
-          </Link>
-        </Col>
-      </Row>
+          <Col>
+            <Link to="/user-profile">
+              <Button className="btn btn-light btn-outline-dark">
+                Crie Perfil
+              </Button>
+            </Link>
+          </Col>
+        </Row>
 
-      {<Row className="d-flex justify-content-evenly mt-4">
-        {user
-          .filter((user) =>
-            user.nome.toLowerCase().includes(search.toLowerCase())
-          )
-          .map((user) => {
-            return (
-              <Card
-                key={user._id}
-                style={{
-                  width: "14rem",
-                  margin: "20px",
-                  alignItems: "center",
-                }}>
-                <Card.Img
-                  variant="top"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVfEJWjl2PyBQKuEoj64zfklCKW9PftoWcXQ&usqp=CAU"
-                />
-                <Card.Body>
-                  <Card.Title>{user.nome}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    {user.sexo}
-                  </Card.Subtitle>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    idade: {user.idade} anos
-                  </Card.Subtitle>
-                </Card.Body>
-                <Card.Footer className="bg-white">
-                  <Link to={`/profile/${user._id}`}>
-                    <Button variant="dark">Perfil Completo</Button>
-                  </Link>
-                </Card.Footer>
-              </Card>
-            );
-          })}
-      </Row>}
-    </div>
+        {
+          <Row className="d-flex justify-content-evenly mt-4">
+            {user
+              .filter((user) =>
+                user.nome.toLowerCase().includes(search.toLowerCase())
+              )
+              .map((user) => {
+                return (
+                  <Card
+                    key={user._id}
+                    style={{
+                      width: "14rem",
+                      margin: "20px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Card.Img
+                      variant="top"
+                      src="https://thumbor.cartpanda.com/4zmdTFchn7WoN3E5SkWqW481nwc=/600x0/https://assets.mycartpanda.com/static/products_images/dDGNEXKGrqaRiJ3n84bwzyW9CFIz0d.jpg"
+                    />
+                    <Card.Body>
+                      <Card.Title>{user.nome}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        {user.sexo}
+                      </Card.Subtitle>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        idade: {user.idade} anos
+                      </Card.Subtitle>
+                    </Card.Body>
+                    <Card.Footer className="bg-white">
+                      <Link to={`/profile/${user._id}`}>
+                        <Button variant="dark">Perfil Completo</Button>
+                      </Link>
+                    </Card.Footer>
+                  </Card>
+                );
+              })}
+          </Row>
+        }
+      </div>
     </>
   );
 }
