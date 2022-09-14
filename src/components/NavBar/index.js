70fd929e3e35443c74c34db2cbd160976e05f5d7
Leonardo Nunes <itsmeleo_n@outlook.com>
2022-09-14 15:47:47 -0300

    Home e Nav finalizadas

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,20 +1,52 @@
+import logo from "../../assets/Logo.png";
+import hamburguinho from "../../assets/Hamburguinho.png";
+import Dropdown from 'react-bootstrap/Dropdown';
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 function NavBar() {
+
   return (
     <div>
       <nav className="navbar shadow-md rounded-bottom">
-        
-        <Link to="/">HomePage</Link>
 
-        <Link to="/profiles">Profiles</Link>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexWrap: "nowrap",
+          flexDirection: "row",
+          marginTop: "18px",
+          marginLeft: "25px",
+          marginRight: "25px",
+        }}
+      >
+      <Link to="/" style={{marginLeft:"0px", width:"48%"}}>
+        <img src={logo} alt="logo" style={{ width: "100%", marginTop:"0px" }}/>
+      </Link>
 
-        <Link to="/allplants">AllPlants</Link>
+        <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+      <img src={hamburguinho} alt="hamburguinho" style={{ width: "100%", marginTop:"0px" }} />
+      </Dropdown.Toggle>
 
-        <Link to="/quizz">Quizz</Link>
-
-        <Link to="/about">AboutUs</Link>
+      <Dropdown.Menu className="dropdown">
+      <Link to="/">
+        <Dropdown.Item href="#/action-1">Início</Dropdown.Item>
+      </Link>
+      <Link to="/profiles">
+        <Dropdown.Item href="#/action-2">Perfis</Dropdown.Item>
+      </Link>
+      <Link to="/allplants">
+        <Dropdown.Item href="#/action-3">Todas as plantas</Dropdown.Item>
+      </Link>
+      <Link to="/about">
+        <Dropdown.Item href="#/action-4">Sobre nós</Dropdown.Item>
+      </Link>
+      </Dropdown.Menu>
+    </Dropdown>
+    
+      </div>
 
       
       </nav>
