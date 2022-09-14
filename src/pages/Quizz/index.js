8ef748d7308d9c60d6dd7c3a88f8a5f31281e0ec
PHMS13 <phms.13@hotmail.com>
2022-09-14 14:33:06 -0300

    array de perguntas no quiz

--- src/pages/Quizz/index.js
+++ src/pages/Quizz/index.js
@@ -2,10 +2,43 @@
 import NavBar from "../../components/NavBar";
 
 function Quizz() {
+
+
+  
+const allquestions = [
+  {
+    pergunta: "Você possui plantas em casa?",
+  
+  },
+  {
+    pergunta: "Com que frequencia você cuida das suas plantas?",
+  
+  },
+  {
+    pergunta: "De 0 a 5, quanta luminosidade tem na sua casa?",
+  
+  },
+  {
+    pergunta: "Normalmente, qual a temperatura da sua casa?",
+  
+  },
+  {
+    pergunta: "O quanto a sua casa é arejada?",
+  
+  },
+  {
+    pergunta: "Com que frequência você rega as suas plantas?",
+  
+  },
+];
+
+
   return (
     <div>
       
       <h1>QUIZZ</h1>
+
+      
     </div>
   );
 }
